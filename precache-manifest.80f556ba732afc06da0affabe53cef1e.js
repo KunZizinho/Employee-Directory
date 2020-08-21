2b9e60bf5a880cc5475329847e42b86635b9ca50
KunZizinho <kun.zizinho@gmail.com>
2020-08-21 09:03:56 -0400

    Updates

---
+++ precache-manifest.80f556ba732afc06da0affabe53cef1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0771560e1d5482270c767d397c465c77",
+    "url": "/KunZizinho/Employee-Directory.git/index.html"
+  },
+  {
+    "revision": "c4c00efd61b778e2a903",
+    "url": "/KunZizinho/Employee-Directory.git/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "50aad4f406c6ea71bec0",
+    "url": "/KunZizinho/Employee-Directory.git/static/js/2.779aeb0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KunZizinho/Employee-Directory.git/static/js/2.779aeb0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4c00efd61b778e2a903",
+    "url": "/KunZizinho/Employee-Directory.git/static/js/main.b3cac90b.chunk.js"
+  },
+  {
+    "revision": "626d6d2bc6727da31a3f",
+    "url": "/KunZizinho/Employee-Directory.git/static/js/runtime-main.c56c1223.js"
+  }
+]);
