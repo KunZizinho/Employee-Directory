6d2237933df42afed35236b6310d061b08632432
KunZizinho <kun.zizinho@gmail.com>
2020-08-21 10:13:35 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/KunZizinho/Employee-Directory.git/precache-manifest.80f556ba732afc06da0affabe53cef1e.js"
+  "/KunZizinho/Employee-Directory/precache-manifest.30e3ac7700d9ed108adcb887a937d9ef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/KunZizinho/Employee-Directory.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/KunZizinho/Employee-Directory/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
